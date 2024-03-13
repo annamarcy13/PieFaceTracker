9e68b55e042ae30f4be14e150bd071ca90de9b49
CMP_AnMarcy <CMP_AnMarcy@RM37-1>
2024-03-13 08:42:04 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,212 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtDonationAmt = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnDonate = New System.Windows.Forms.Button()
+        Me.CBOStudents = New System.Windows.Forms.ComboBox()
+        Me.LblstudentAmt = New System.Windows.Forms.Label()
+        Me.LstStudentDonations = New System.Windows.Forms.ListBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.LblStudentTotal = New System.Windows.Forms.Label()
+        Me.BtnRemove = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.LblClassTotal = New System.Windows.Forms.Label()
+        Me.LblHigh = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(51, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Name"
+        '
+        'TxtDonationAmt
+        '
+        Me.TxtDonationAmt.Location = New System.Drawing.Point(410, 19)
+        Me.TxtDonationAmt.Multiline = True
+        Me.TxtDonationAmt.Name = "TxtDonationAmt"
+        Me.TxtDonationAmt.Size = New System.Drawing.Size(120, 20)
+        Me.TxtDonationAmt.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(272, 17)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(132, 20)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Donation amount"
+        '
+        'btnDonate
+        '
+        Me.btnDonate.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnDonate.Location = New System.Drawing.Point(563, 19)
+        Me.btnDonate.Name = "btnDonate"
+        Me.btnDonate.Size = New System.Drawing.Size(146, 37)
+        Me.btnDonate.TabIndex = 4
+        Me.btnDonate.Text = "Add donation"
+        Me.btnDonate.UseVisualStyleBackColor = True
+        '
+        'CBOStudents
+        '
+        Me.CBOStudents.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.CBOStudents.FormattingEnabled = True
+        Me.CBOStudents.Location = New System.Drawing.Point(83, 21)
+        Me.CBOStudents.Name = "CBOStudents"
+        Me.CBOStudents.Size = New System.Drawing.Size(121, 21)
+        Me.CBOStudents.TabIndex = 5
+        '
+        'LblstudentAmt
+        '
+        Me.LblstudentAmt.AutoSize = True
+        Me.LblstudentAmt.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblstudentAmt.Location = New System.Drawing.Point(79, 65)
+        Me.LblstudentAmt.Name = "LblstudentAmt"
+        Me.LblstudentAmt.Size = New System.Drawing.Size(0, 20)
+        Me.LblstudentAmt.TabIndex = 6
+        '
+        'LstStudentDonations
+        '
+        Me.LstStudentDonations.FormattingEnabled = True
+        Me.LstStudentDonations.Location = New System.Drawing.Point(16, 65)
+        Me.LstStudentDonations.Name = "LstStudentDonations"
+        Me.LstStudentDonations.Size = New System.Drawing.Size(188, 69)
+        Me.LstStudentDonations.TabIndex = 7
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(181, 270)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 20)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Student Total:"
+        '
+        'LblStudentTotal
+        '
+        Me.LblStudentTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblStudentTotal.Location = New System.Drawing.Point(296, 270)
+        Me.LblStudentTotal.Name = "LblStudentTotal"
+        Me.LblStudentTotal.Size = New System.Drawing.Size(77, 20)
+        Me.LblStudentTotal.TabIndex = 9
+        Me.LblStudentTotal.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'BtnRemove
+        '
+        Me.BtnRemove.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnRemove.Location = New System.Drawing.Point(16, 140)
+        Me.BtnRemove.Name = "BtnRemove"
+        Me.BtnRemove.Size = New System.Drawing.Size(119, 32)
+        Me.BtnRemove.TabIndex = 10
+        Me.BtnRemove.Text = "Remove"
+        Me.BtnRemove.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(180, 229)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(163, 26)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Total Donations"
+        '
+        'LblClassTotal
+        '
+        Me.LblClassTotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblClassTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblClassTotal.Location = New System.Drawing.Point(349, 228)
+        Me.LblClassTotal.Name = "LblClassTotal"
+        Me.LblClassTotal.Size = New System.Drawing.Size(163, 26)
+        Me.LblClassTotal.TabIndex = 12
+        '
+        'LblHigh
+        '
+        Me.LblHigh.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.LblHigh.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.LblHigh.Location = New System.Drawing.Point(563, 68)
+        Me.LblHigh.Name = "LblHigh"
+        Me.LblHigh.Size = New System.Drawing.Size(163, 158)
+        Me.LblHigh.TabIndex = 13
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(437, 68)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(120, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Highest Donors"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(764, 318)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.LblHigh)
+        Me.Controls.Add(Me.LblClassTotal)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.BtnRemove)
+        Me.Controls.Add(Me.LblStudentTotal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.LstStudentDonations)
+        Me.Controls.Add(Me.LblstudentAmt)
+        Me.Controls.Add(Me.CBOStudents)
+        Me.Controls.Add(Me.btnDonate)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtDonationAmt)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtDonationAmt As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnDonate As Button
+    Friend WithEvents CBOStudents As ComboBox
+    Friend WithEvents LblstudentAmt As Label
+    Friend WithEvents LstStudentDonations As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents LblStudentTotal As Label
+    Friend WithEvents BtnRemove As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents LblClassTotal As Label
+    Friend WithEvents LblHigh As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,193 @@
+﻿
+Imports System.Security.Cryptography
+Imports System.Windows.Forms.ComponentModel.Com2Interop
+Imports Newtonsoft.Json
+
+Public Class Form1
+    Private Const strSTUDENTFILE As String = "StudentList.json"
+    Private studenttable As New List(Of Student)
+    Private names() As String = {"Aiden", "Alexander", "Anna", "Anthony", "Arlo", "Benjamin", "Bradley", "Brooks", "Chevy", "Collin", "Francis", "Holly", "Logan", "Matthew", "Michael", "Nathan", "Preston", "Srikar", "Tanmay", "Vincent", "Walker", "Zeph"}
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        PopulateStudentTable()
+        populatestudentcombobox()
+    End Sub
+    Private Sub populatestudentcombobox()
+        CBOStudents.Items.AddRange(studenttable.ToArray)
+    End Sub
+
+    Private Sub PopulateStudentTable()
+        If IO.File.Exists(strSTUDENTFILE) Then
+            loadStudentTableFromFIle()
+        Else
+            StartFresh()
+        End If
+    End Sub
+    Private Sub loadStudentTableFromFIle()
+
+        Dim reader As New IO.StreamReader(strSTUDENTFILE)
+        Dim str As String = reader.ReadToEnd
+        studenttable = JsonConvert.DeserializeObject(Of List(Of Student))(str)
+        reader.Close()
+
+    End Sub
+    Private Sub StartFresh()
+        For Each s As String In names
+            'Add a new student object to our dictionary
+            'For know we will use the first name as the key because this list is unique
+            studenttable.Add(New Student(s, New List(Of Donation)))
+        Next
+    End Sub
+
+    Private Sub CBOStudents_SelectedIndexChanged(sender As Object, e As EventArgs) Handles CBOStudents.SelectedIndexChanged
+        UpdateValues()
+    End Sub
+
+    Private Sub UpdateValues()
+        Dim s As Student = CBOStudents.SelectedItem
+        If Not IsNothing(s) Then
+            loadDonationListBox()
+            LblStudentTotal.Text = s.Getotaldonations().ToString("C2")
+            LblClassTotal.Text = GetClassTotalDOnations.ToString("C2")
+        End If
+        PopulateTopDonors()
+    End Sub
+    Private Sub PopulateTopDonors()
+        LblHigh.Text = ""
+        studenttable.Sort(Function(x, y) y.Getotaldonations.CompareTo(x.Getotaldonations))
+        Dim i As Integer = 0
+        While i < 5 AndAlso studenttable(i).Getotaldonations > 0
+            Dim s As Student = studenttable(i)
+            LblHigh.Text = $"{LblHigh.Text}{s.Name.PadRight(10)} {s.Getotaldonations.ToString("C2").PadLeft(8)}{vbNewLine}"
+            i += 1
+        End While
+
+    End Sub
+
+    Private Function GetClassTotalDOnations() As Double
+        Dim total As Double = 0
+
+
+        For Each s As Student In studenttable
+            total += s.Getotaldonations
+
+        Next
+        Return total
+    End Function
+
+
+
+
+    Private Sub loadDonationListBox()
+        LstStudentDonations.Items.Clear()
+        Dim s As Student = CBOStudents.SelectedItem
+        LstStudentDonations.Items.AddRange(s.Dontations.ToArray)
+
+    End Sub
+
+    Private Sub BtnDonate_Click(sender As Object, e As EventArgs) Handles btnDonate.Click
+        Dim amt As Double
+        Dim valid As Boolean = Double.TryParse(TxtDonationAmt.Text, amt)
+        If valid Then
+            Dim s As Student = CBOStudents.SelectedItem
+            s.Dontations.Add(New Donation(amt, Date.Now))
+        End If
+        UpdateValues()
+    End Sub
+    Private Sub savefiles()
+        Dim json = JsonConvert.serializeobject(CBOStudents.Items)
+        Dim writer = New IO.StreamWriter(strSTUDENTFILE)
+        writer.Write(json)
+        writer.Close()
+
+    End Sub
+
+    Private Sub Form1_FormClosed(sender As Object, e As FormClosedEventArgs) Handles Me.FormClosed
+        savefiles()
+
+    End Sub
+
+    Private Sub BtnRemove_Click(sender As Object, e As EventArgs) Handles BtnRemove.Click
+        Dim s As Student = CBOStudents.SelectedItem
+        Dim d As Donation = LstStudentDonations.SelectedItem
+        If Not IsNothing(s) AndAlso Not IsNothing(d) Then
+            s.Dontations.Remove(d)
+            UpdateValues()
+        End If
+
+
+
+    End Sub
+End Class
+
+Public Class Student
+    Private _name As String
+    Private _dontations As List(Of Donation)
+
+    Public Sub New(name As String, donations As List(Of Donation))
+        Me.Name = name
+        Me.Dontations = donations
+    End Sub
+    Public Property Name As String
+        Get
+            Return _name
+        End Get
+        Set(value As String)
+            _name = value
+        End Set
+    End Property
+
+    Public Property Dontations As List(Of Donation)
+        Get
+            Return _dontations
+        End Get
+        Set(value As List(Of Donation))
+            _dontations = value
+        End Set
+    End Property
+    Public Function Getotaldonations() As Double
+        Dim total As Double = 0
+        For Each d As Donation In Dontations
+            total += d.Amount
+
+        Next
+
+        Return total
+    End Function
+    Public Overrides Function ToString() As String
+        Return Name
+    End Function
+End Class
+
+Public Class Donation
+    Private _amount As Double
+    Private _theDate As Date
+
+    Public Property Amount As Double
+        Get
+            Return _amount
+        End Get
+        Set(value As Double)
+            _amount = value
+        End Set
+    End Property
+
+    Public Property TheDate As Date
+        Get
+            Return _theDate
+        End Get
+        Set(value As Date)
+            _theDate = value
+        End Set
+    End Property
+
+    Public Sub New(amount As Double, theDate As Date)
+        Me.Amount = amount
+        Me.TheDate = theDate
+    End Sub
+
+    Public Overrides Function tostring() As String
+        Return Amount.ToString("C2").PadRight(10) & Me.TheDate.ToLocalTime
+
+    End Function
+
+End Class
